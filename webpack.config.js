6f2c2d047fe6e1e838db8b3c1b86bdda08d4e976
Natali258 <natalya.holyavko@gmail.com>
2024-03-21 22:40:18 +0200

    webpack-02

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js", // Вказуємо вхідний файл
   output: {
     path: path.resolve(__dirname, "dist"), // Вказуємо шлях для збірки
-    filename: "bundle.js", // Вказуємо назву зібраного файлу
+    filename: "[name].[contenthash].js", // Вказуємо назву зібраного файлу
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Вказуємо, які файли потрібно обробляти
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader", // Використовуємо Babel для трансляції ES6+ у старішу версію JavaScript
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.css$/, // Обробка файлів CSS
-        use: ["style-loader", "css-loader"], // Використання style-loader для включення стилів у HTML та css-loader для обробки CSS
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/, // Обробка зображень
-        use: ["file-loader"], // Використання file-loader для завантаження файлів
-      },
-    ],
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/, // Вказуємо, які файли потрібно обробляти
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: "babel-loader", // Використовуємо Babel для трансляції ES6+ у старішу версію JavaScript
+  //         options: {
+  //           presets: ["@babel/preset-env"],
+  //         },
+  //       },
+  //     },
+  //     {
+  //       test: /\.css$/, // Обробка файлів CSS
+  //       use: ["style-loader", "css-loader"], // Використання style-loader для включення стилів у HTML та css-loader для обробки CSS
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/, // Обробка зображень
+  //       use: ["file-loader"], // Використання file-loader для завантаження файлів
+  //     },
+  //   ],
+  // },
 };
