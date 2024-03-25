2e2b69bc41881ef991098bf8c79806622040d53c
Natali258 <natalya.holyavko@gmail.com>
2024-03-25 21:40:09 +0200

    webpack-babel-js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 import "../index.html";
+import { mult, sum } from "../modules/calc.js";
+import "../style.css";
+
+console.log(mult(3, 5));
+console.log(sum(3, 5));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  devServer: {
+    port: 3000,
+    open: true,
+  },
   entry: "./src/js/index.js", // Вказуємо вхідний файл
   output: {
     path: path.resolve(__dirname, "dist"), // Вказуємо шлях для збірки
@@ -10,6 +15,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
     }),
   ],
   module: {
@@ -20,7 +28,28 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("postcss-preset-env")],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
       // {
       //   test: /\.css$/, // Обробка файлів CSS
